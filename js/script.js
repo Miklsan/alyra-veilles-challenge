5909287f14cb68c606ef714ade7d533449574029
dragon-s20 <promoetherjs.dragon-s20@gmail.com>
2020-07-31 20:14:12 +0200

    Fusion branche Rémy et Stef_date moment ok_Dates veilles à venir ok mais non > a moment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 "use strict"
+
+// Partie Stef
+
+const moment = require("moment");
 const dateNow = moment();
 dateNow.locale("fr");
 console.log("dddd DD/MM/YYYY", dateNow.format("dddd DD/MM/YYYY"));
@@ -10,3 +14,36 @@ p.textContent = `Bonjour ! Nous sommes le ${dateNow.format(
 //p.style.backgroundColor = "blue";
 
 h1.after(p);
+
+console.log(dateNow);
+
+// Partie Rémy
+function alldates(entries) {
+  let date = [];
+  for (let entry of entries) {
+    if ("date" in entry) {
+      for (let entry of entries) {
+        if (!date.includes(items) > dateNow) {
+          date.push(items);
+        }
+      }
+    }
+    console.log("entry.date", entry.date);
+  }
+}
+alldates();
+
+console.log(alldates(entries));
+
+/*for (let entry of entries) {
+    console.log("entry.date", entry.date);
+  }
+  if (entry.date < "dateNow") {
+    return arrayOfDDMMYYYY.sort(
+      (k1, k2) => moment(k1, dateFormat) - moment(k2, dateFormat)
+    );
+  }
+}
+sortByDate();
+
+console.log("sortByDate", sortByDate(entries));*/
