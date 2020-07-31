1f2edc60ff9eff70bea8cf9dbfff18800c90d3ed
Remy-Mil <milazzo.remy@gmail.com>
2020-07-31 20:22:35 +0200

    ajout partie veilles à venir

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let filterEntries = "toutes les veilles "
+
+function insertVeille() {
+    const ulEl = document.createElement("ul")
+    const gridContainer = document.getElementById("grid-container")
+    ulEl.classList.add("row", "list-unstyled")
+    const filteredEntries = entries.filter((el) => {
+        if (filterEntries === "toutes les veilles") {
+            return true
+        } else {
+            return el.category(filterEntries)
+        }
+    })
+    for (let veille of filteredEntries) {
+        const li = document.createElement("li")
+        li.classList.add("col-sm-12", "col-lg-12")
+        li.innerHTML = <div class="card mr-5 ml-5 mb-2 shadow-sm bg-white rounded">
+        <div class="card-body">
+            <h5 class="card-title ">${veille.subject}</h5>
+            <a class="badge bg-primary text-decoration-none" href="#" role="button">${veille.category}</a>
+            <p id="date" class=" card-text"><small class="text-muted"></small>${veille.date}</p>
+        </div>
+    </div>
+        ulEl.append(li)
+    }
+    gridContainer.innerHTML = ""
+    gridContainer.append(ulEl)
+}
+insertVeille()
+
+//Parti Rémy
+
+const moment = require("moment");
+const dateNow = moment();
+
+function alldates(entries) {
+  let date = [];
+  for (let entry of entries) {
+    if ("date" in entry) {
+      for (let entry of entries) {
+        if (!date.includes(items) > dateNow) {
+          date.push(items);
+        }
+      }
+    }
+    console.log("entry.date", entry.date);
+  }
+}
+alldates();
+
+console.log(alldates(entries));
